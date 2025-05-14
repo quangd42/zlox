57c59c3631414ea7a6a98ae19d1e647161905d18
quangd42 <quangdn42@gmail.com>
2025-05-14 07:58:58 -0400

    refactor stack operations

--- src/vm.zig
+++ src/vm.zig
@@ -6,15 +6,16 @@ const dbg = @import("config").@"debug-trace";
 
 const _chunk = @import("chunk.zig");
 const Chunk = _chunk.Chunk;
+const OpCode = _chunk.OpCode;
 const Compiler = @import("compiler.zig").Compiler;
 const debug = @import("debug.zig");
 const value = @import("value.zig");
 const Value = value.Value;
 
 pub const InterpretError = error{
-    COMPILE_ERROR,
-    RUNTIME_ERROR,
-};
+    CompileError,
+    RuntimeError,
+} || Allocator.Error;
 
 pub const VM = struct {
     chunk: *Chunk = undefined,
@@ -67,18 +68,21 @@ pub const VM = struct {
         defer self.allocator.free(source);
 
         self.interpret(source) catch |err| switch (err) {
-            InterpretError.COMPILE_ERROR => exit(65),
-            InterpretError.RUNTIME_ERROR => exit(70),
+            InterpretError.CompileError => exit(65),
+            InterpretError.RuntimeError => exit(70),
+            InterpretError.OutOfMemory => exit(70),
         };
         exit(0);
     }
 
-    fn interpret(self: *VM, source: []const u8) InterpretError!void {
+    fn interpret(self: *VM, source: []const u8) !void {
+        self.resetStack();
+
         var chunk = Chunk.init(self.allocator);
         defer chunk.deinit();
 
         var compiler = Compiler.init(self.allocator, source, &chunk);
-        compiler.compile() catch return InterpretError.COMPILE_ERROR;
+        compiler.compile() catch return InterpretError.CompileError;
 
         self.chunk = &chunk;
         self.ip = 0;
@@ -86,10 +90,49 @@ pub const VM = struct {
         return self.run();
     }
 
-    // TODO:
-    fn run(self: *VM) InterpretError!void {
-        _ = self;
-        return;
+    fn run(self: *VM) !void {
+        while (self.ip < self.chunk.code.items.len) {
+            if (dbg) {
+                std.debug.print("          ", .{});
+                for (self.stack.items) |slot| {
+                    std.debug.print("[ {} ]", .{slot});
+                }
+                std.debug.print("\n", .{});
+                _ = debug.disassembleInstruction(self.chunk, self.ip);
+            }
+            const instruction: _chunk.OpCode = @enumFromInt(self.readByte());
+            switch (instruction) {
+                .CONSTANT => try self.push(self.readConstant()),
+                .ADD => try self.binaryOp(.ADD),
+                .SUBTRACT => try self.binaryOp(.SUBTRACT),
+                .MULTIPLY => try self.binaryOp(.MULTIPLY),
+                .DIVIDE => try self.binaryOp(.DIVIDE),
+                .NEGATE => {
+                    switch (self.peek(0)) {
+                        .Number => {
+                            const val = self.pop().?;
+                            try self.push(.{ .Number = -val.Number });
+                        },
+                        else => {
+                            self.runtimeError("Operand must be a number.", .{});
+                            return InterpretError.RuntimeError;
+                        },
+                    }
+                },
+                .RETURN => {
+                    std.debug.print("{}\n", .{self.pop().?});
+                    return;
+                },
+                // missing OP_CONSTANT_LONG
+                else => return InterpretError.CompileError,
+            }
+        }
+    }
+
+    fn runtimeError(self: *VM, comptime fmt: []const u8, args: anytype) void {
+        std.debug.print(fmt, args);
+        std.debug.print("\n[line {d}] in script\n", .{self.chunk.lines.items[self.ip - 1]});
+        self.resetStack();
     }
 
     fn readByte(self: *VM) u8 {
@@ -102,15 +145,34 @@ pub const VM = struct {
         return self.chunk.constants.items[constant_idx];
     }
 
-    fn binaryOp(self: *VM, comptime op: enum { add, subtract, multiply, divide }) !void {
-        const b = self.stack.pop().?;
-        const a = self.stack.pop().?;
+    fn peek(self: *VM, distance: usize) Value {
+        const len = self.stack.items.len;
+        const idx = if (distance > len - 1) 0 else len - 1 - distance;
+        return self.stack.items[idx];
+    }
 
-        try self.stack.append(switch (op) {
-            .add => a + b,
-            .subtract => a - b,
-            .multiply => a * b,
-            .divide => a / b,
+    fn pop(self: *VM) ?Value {
+        return self.stack.pop();
+    }
+
+    fn push(self: *VM, val: Value) !void {
+        return self.stack.append(val);
+    }
+
+    fn resetStack(self: *VM) void {
+        self.stack.clearRetainingCapacity();
+    }
+
+    fn binaryOp(self: *VM, comptime op: OpCode) !void {
+        const b = self.pop().?.asNumber() orelse return InterpretError.RuntimeError;
+        const a = self.pop().?.asNumber() orelse return InterpretError.RuntimeError;
+
+        try self.push(switch (op) {
+            .ADD => .{ .Number = a + b },
+            .SUBTRACT => .{ .Number = a - b },
+            .MULTIPLY => .{ .Number = a * b },
+            .DIVIDE => .{ .Number = a / b },
+            else => unreachable,
         });
     }
 };
